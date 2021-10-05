65c016e0dad67640142ed77c4b0fec1272cfd1f6
baothanh <hlb.thanh20091998@gmail.com>
2021-10-05 17:29:03 +0700

    update cv

--- src/main/page/home/home.page.jsx
+++ src/main/page/home/home.page.jsx
@@ -3,6 +3,8 @@ import ParticleComponent from '../../component/particle/particle.component'
 import "./home.style.scss"
 import fb from "../../img/contact/facebook_logos_PNG19753.png"
 import git from "../../img/contact/github_PNG2.png"
+import cv from "../../img/cv_PNG6.png"
+import cvFile from "../../img/IT_FRONEND_HOANGLEBAOTHANH.pdf"
 
 export default function Home() {
     window.scrollTo(0, 0)
@@ -18,6 +20,9 @@ export default function Home() {
                 </a>
                 <a href="https://github.com/baothanh20091998" target="_blank">
                     <img src={git} style={{ marginLeft: "10px", width: "45px", height: "45px", borderRadius: "50%" }} alt="" />
+                </a>
+                <a href={cvFile} target="_blank">
+                    <img src={cv} style={{ marginLeft: "10px", width: "45px", height: "45px", borderRadius: "50%" }} alt="" />
                 </a>
             </div>
         </>
